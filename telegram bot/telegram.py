2d666059310ea1513e0ddb4cf7e7e02cf4a4b999
Sudheeshspai <115912414+Sudheeshspai@users.noreply.github.com>
2023-10-16 20:44:10 +0530

    Add files via upload

---
+++ telegram bot/telegram.py
@@ -0,0 +1,6 @@
+import telegram.ext
+
+Token= "6498475454:AAHRAjfREGvQQOlO0fmf-Dr82SIpDu7eY7c"
+
+updater = telegram.ext.Updater (" 6498475454:AAHRAjfREGvQQOlO0fmf-Dr82SIpDu7eY7c " use_context=True)
+  
